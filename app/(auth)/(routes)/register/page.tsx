4a1642b51620ce6ce69a58be3ee89e6f7d5749bf
godbrand69 <you@example.com>
2024-01-01 13:48:11 +0530

    auth

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-    return (  
-        <div className="bg-red">
-            Register
-        </div>
-    );
-}
- 
-export default RegisterPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-4xl flex flex-col text-center font-bold text-indigo-500'>
-      Clone
-      <Button>Nigga</Button>
-    </div>
+    <UserButton afterSignOutUrl='/' />
   )
 }
