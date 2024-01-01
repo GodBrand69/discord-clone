0517fb600c570165a1fb78af1a0b9dbe5fc8008e
godbrand69 <you@example.com>
2024-01-01 13:15:10 +0530

    first commit

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+    return (  
+        <div className="bg-red">
+            Register
+        </div>
+    );
+}
+ 
+export default RegisterPage;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='text-4xl flex flex-col text-center font-bold text-indigo-500'>
+      Clone
+      <Button>Nigga</Button>
+    </div>
+  )
+}
